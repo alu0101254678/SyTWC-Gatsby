2960049b3e32dee2051cde39d6add997135a36ec
Yago Perez Molanes <alu0101254678@ull.edu.es>
2024-01-05 09:53:03 +0000

    Github Pages

--- gatsby-building-component/gatsby-config.js
+++ gatsby-building-component/gatsby-config.js
@@ -2,8 +2,12 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
+  pathPrefix: `/SyTWC-Gatsby`,
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
+  },
+  siteMetadata: {
+    siteUrl: `https://alu0101254678.github.io/SyTWC-Gatsby`,
   },
   plugins: [
     `gatsby-plugin-image`,
